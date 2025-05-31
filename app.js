149986535013c7de27b0dd9355cf826f90a731ee
Adriaan Pienaar <112950444+Adriaan0108@users.noreply.github.com>
2025-05-31 19:52:05 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  if (storedTasks) {
+    storedTasks.forEach((task) => tasks.push(task));
+    updateTaskList();
+  }
+});
+
+let tasks = [];
+
+const saveTasks = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const addTask = () => {
+  const taskText = document.getElementById("taskText");
+
+  const text = taskText.value.trim();
+
+  if (text) {
+    tasks.push({ text: text, completed: false });
+    taskText.value = "";
+    updateTaskList();
+    // updateStats();
+  }
+};
+
+const toggleTaskComplete = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  updateTaskList();
+  //   updateStats();
+};
+
+const deleteTask = (index) => {
+  tasks.splice(index, 1);
+  updateTaskList();
+  //   updateStats();
+};
+
+const editTask = (index) => {
+  document.getElementById("taskText").focus();
+
+  const input = document.getElementById("taskText");
+  input.value = tasks[index].text;
+
+  deleteTask(index);
+  updateTaskList();
+  //   updateStats();
+};
+
+const updateStats = () => {
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const totalTasks = tasks.length;
+
+  const progress = (completedTasks / totalTasks) * 100;
+  const progressBar = document.getElementById("progress");
+  progressBar.style.width = `${progress}%`;
+
+  document.getElementById(
+    "numbers"
+  ).innerText = `${completedTasks} / ${totalTasks}`;
+};
+
+const updateTaskList = () => {
+  const list = document.getElementById("task-list");
+
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const item = document.createElement("li");
+
+    item.innerHTML = `
+     <div class="taskItem">
+      <div class="task ${task.completed ? "completed" : ""}">
+        <input type="checkbox" class="checkbox" ${
+          task.completed ? "checked" : ""
+        }/>
+        <p>${task.text}</p>
+      </div>
+      <div class="icons">
+        <img src="./img/edit.png" onClick="editTask(${index})"/>
+        <img src="./img/delete.png" onClick="deleteTask(${index})"/>
+      </div>
+    </div>
+    `;
+
+    item.addEventListener("change", () => toggleTaskComplete(index));
+
+    // Add event listener specifically to the checkbox
+    // const checkbox = item.querySelector(".checkbox");
+    // checkbox.addEventListener("change", () => toggleTaskComplete(index));
+
+    list.appendChild(item);
+  });
+
+  updateStats();
+  saveTasks();
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+  e.preventDefault(); // prevent default behavior of page reloading
+
+  addTask();
+});
