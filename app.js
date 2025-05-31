e61a58ab7db021ad3fa5f5cb119471f49d6dc5d4
Adriaan Pienaar <112950444+Adriaan0108@users.noreply.github.com>
2025-05-31 20:05:02 +0200

    Added PWA functionality

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// Service Worker Registration
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
